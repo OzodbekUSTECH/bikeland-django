5a842320196847412c59135f6654216734653419
LittleMoki <steamlittle659@gmail.com>
2023-12-23 12:51:33 +0500

    section main and catalog and icon logo

--- bikeland/app/views.py
+++ bikeland/app/views.py
@@ -20,22 +20,80 @@ def home(request):
             {
                 'img': '../static/images/mainConfidence/handshake.svg',
                 'title': 'Опыт',
-                'mainSubtitle':'10',
+                'mainSubtitle': '10',
                 'subtitle': 'лет На рынке Узбекистана'
             },
             {
                 'img': '../static/images/mainConfidence/bike.svg',
                 'title': 'Профессионализм',
-                'mainSubtitle':'40+',
+                'mainSubtitle': '40+',
                 'subtitle': 'Моделей Мото-техники в наличии'
             },
             {
                 'img': '../static/images/mainConfidence/shade.svg',
                 'title': 'Доверие',
-                'mainSubtitle':'3000+',
+                'mainSubtitle': '3000+',
                 'subtitle': 'Счастливых обладателей нашей Мото-техники'
             },
 
+        ],
+        'catalogContent': [
+            {
+                'img': '../static/images/cardCatalog/bike.jpg',
+                'tag': '',
+                'title': 'CF MOTO 800MT (CF800-5)',
+                'subtitle': 'N6003732',
+                'price': '260 329 500 UZS'
+            },
+            {
+                'img': '../static/images/cardCatalog/bike.jpg',
+                'tag': 'Новинка в наличии',
+                'title': 'CF MOTO 800MT (CF800-5)',
+                'subtitle': 'N6003732',
+                'price': '260 329 500 UZS'
+            },
+            {
+                'img': '../static/images/cardCatalog/bike.jpg',
+                'tag': 'Новинка в наличии',
+                'title': 'CF MOTO 800MT (CF800-5)',
+                'subtitle': 'N6003732',
+                'price': '260 329 500 UZS'
+            },
+            {
+                'img': '../static/images/cardCatalog/bike.jpg',
+                'tag': 'Новинка в наличии',
+                'title': 'CF MOTO 800MT (CF800-5)',
+                'subtitle': 'N6003732',
+                'price': '260 329 500 UZS'
+            },
+            {
+                'img': '../static/images/cardCatalog/bike.jpg',
+                'tag': 'Новинка в наличии',
+                'title': 'CF MOTO 800MT (CF800-5)',
+                'subtitle': 'N6003732',
+                'price': '260 329 500 UZS'
+            },
+            {
+                'img': '../static/images/cardCatalog/bike.jpg',
+                'tag': 'Новинка в наличии',
+                'title': 'CF MOTO 800MT (CF800-5)',
+                'subtitle': 'N6003732',
+                'price': '260 329 500 UZS'
+            },
+            {
+                'img': '../static/images/cardCatalog/bike.jpg',
+                'tag': 'Новинка в наличии',
+                'title': 'CF MOTO 800MT (CF800-5)',
+                'subtitle': 'N6003732',
+                'price': '260 329 500 UZS'
+            },
+            {
+                'img': '../static/images/cardCatalog/bike.jpg',
+                'tag': 'Новинка в наличии',
+                'title': 'CF MOTO 800MT (CF800-5)',
+                'subtitle': 'N6003732',
+                'price': '260 329 500 UZS'
+            },
         ]
     }
     return render(request, "home.html", data)
